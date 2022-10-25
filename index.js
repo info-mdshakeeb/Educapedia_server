7b7e1d8061873b5366655d88913e2f0e74592670
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-10-25 13:08:26 +0600

    data add description

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const experss = require('express')
+const cors = require('cors');
+const courses = require('./Data/courses.json')
 const app = experss()
 const port = 1100;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('node is r')
 })
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id
+    const course = courses.find(rs => rs.id == id)
+    res.send(course)
+})
+
+
 app.listen(port, () => console.log('node is reaning', { port })
 )
